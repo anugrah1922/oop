9f5a806af39a0c5215dc4b01b3bbe6c9dd85bd50
anugrah1922 <62147732+anugrah1922@users.noreply.github.com>
2020-03-13 22:27:48 +0700

    Add files via upload
    tugas 1

---
+++ Mahasiswaa.java
@@ -0,0 +1,16 @@
+public class Mahasiswaa{
+	public static void main(String[] args) {
+		String nim;
+		String nama;
+		String kelas;
+
+		nim = "18090121";
+		nama = "alvin anugrah arrafi";
+		kelas = "kelas 4C";
+
+		System.out.println("=====================");
+		System.out.println("Nim :" + nim);
+		System.out.println("Nama :" + nama);
+		System.out.println("Kelas :" + kelas);	
+	}
+}
